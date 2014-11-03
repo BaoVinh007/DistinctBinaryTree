24c219b4e5c7a537aef75440c06cec7d9ad8c043
VinhNguyen <baovinh007@yahoo.com>
2014-11-02 22:59:05 -0800

    DistinctBinaryTree
    This is an example of the program running on the computer’s console. In
    this example, values of n=3, n=4, and n=5 are used to find out how many
    distinct binary trees are possible as well as a representation of the
    trees.
    EX: With n = 3. The program output corresponds to the binary tree as
    follows: a) 123, b) 132, c) 213, d) 312, and e) 321.

---
+++ BTreeCSharp/BTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace BTreeCSharp
+{
+    class BTreeNode
+    {
+        public int data;
+        public BTreeNode left;
+        public BTreeNode right;
+        public BTreeNode(int d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+    };
+    class BTree
+    {
+        // function constructBTree: generate search binary tree with root = 1, 2,...,n 
+        // 2 paramaters: start = 1, and n is number of nodes 
+        // using vector<BTreeNode*> to store all binary tree that can generate
+        // using vectors because their size can change dynamically
+
+        public ArrayList constructBTree(int start, int n)
+        {
+            ArrayList result = new ArrayList();
+            if (start > n)
+            {
+                // function push_back : add element at the end
+                result.Add(null);
+
+                return result;
+            }
+            int i = start;
+            // Build all possible search binary tree with root = 1, 2, ..., n
+            while (i <= n)
+            {
+                ArrayList left = constructBTree(start, i - 1);
+                ArrayList right = constructBTree(i + 1, n);
+
+                for (int l = 0; l < left.Count; l++)
+                {
+                    for (int r = 0; r < right.Count; r++)
+                    {
+                        BTreeNode node = new BTreeNode(i);
+                        node.left = (BTreeNode)left[l];
+                        node.right = (BTreeNode)right[r];
+                        result.Add(node);
+                    }
+                }
+                i++;
+            }
+            return result;
+        }
+
+
+        // Output binary trees by preorder
+
+        public void outputTree(BTreeNode t)
+        {
+            if (t != null)
+            {
+                Console.Write(t.data);
+                outputTree(t.left);
+                outputTree(t.right);
+            }
+        }
+
+    }
+
+}
---
+++ BTreeCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace BTreeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("C# Version:");
+            do
+            {
+                Console.Write("\nEnter value of n = ");
+                string str = Console.ReadLine();
+                n = Int16.Parse(str);
+                BTree t = new BTree();
+                ArrayList l = t.constructBTree(1, n);
+                Console.WriteLine("All possible distinct search binary trees are " + l.Count);
+                Console.WriteLine("In order(Left < Root < Right)");
+
+
+
+                int i = 0;
+                while (i < l.Count)
+                {
+                    t.outputTree((BTreeNode)l[i]);
+                    i++;
+                    if (i != l.Count)
+                        Console.Write("---");
+                }
+                Console.WriteLine();
+                Console.Write("\nEnter 0 to exit program, otherwise enter 1 to continue: ");
+                str = Console.ReadLine();
+                n = Int16.Parse(str);
+            } while (n != 0);
+        }
+    }
+}
